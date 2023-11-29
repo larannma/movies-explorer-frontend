3c61073fae492007cde33ee93358ed7657d77c7f
Анна Ларионова <Larionova@bro.local>
2023-11-29 18:23:51 +0300

    checked: main for pixel perfect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App root">
       <Header/>
-      <Main/>
-      {/* <Movies/> */}
+      {/* <Main/> */}
+      <Movies/>
       <Footer/>
     </div>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,8 +3,8 @@ import './Navigation.css'
 function Navigation({ isBurgerMenu }) {
     return (
     <div className={`navigation ${isBurgerMenu ? 'navigation_side' : ''}`}>
-        <a href='#main' className={`navigation__item ${isBurgerMenu ? 'navigation__item_theme_light' : ''}`}>Фильмы</a>
-        <a href='#main' className={`navigation__item ${isBurgerMenu ? 'navigation__item_theme_light' : ''}`}>Сохранённые фильмы</a>
+        <a href='#main' className={`navigation__item ${isBurgerMenu ? 'navigation__item_theme_light navigation__bold' : ''}`}>Фильмы</a>
+        <a href='#main' className={`navigation__item ${isBurgerMenu ? 'navigation__item_theme_light navigation__bold' : ''}`}>Сохранённые фильмы</a>
     </div>
     )
 }
